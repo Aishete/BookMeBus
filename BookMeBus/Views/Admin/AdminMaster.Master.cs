e0bd464f914b975d9ea03d7a0c235a307c5b352f
SPC-Store <SPC-Store@DESKTOPS24>
2024-06-25 17:07:58 +0700

    icon, button in home page. colore, plus sign and celander.

--- BookMeBus/Views/Admin/AdminMaster.Master.cs
+++ BookMeBus/Views/Admin/AdminMaster.Master.cs
@@ -13,5 +13,30 @@ namespace BookMeBus.Views.Admin
         {
 
         }
+
+        protected System.Void Menu1_MenuItemClick(System.Object sender, System.Web.UI.WebControls.MenuEventArgs e)
+        {
+
+        }
+
+        protected System.Void Menu1_MenuItemClick1(System.Object sender, System.Web.UI.WebControls.MenuEventArgs e)
+        {
+
+        }
+
+        protected System.Void Page_Load(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected System.Void TextBox1_TextChanged(System.Object sender, System.EventArgs e)
+        {
+
+        }
+
+        protected System.Void DataList1_SelectedIndexChanged()
+        {
+
+        }
     }
 }
---
+++ BookMeBus/Views/Admin/HomePage.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="BookMeBus.Views.Admin.HomePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
